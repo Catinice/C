6c8aed351842cc9ab713db9ad55666bcbd797554
Fastboot <premdevgan9@gmail.com>
2021-01-05 00:12:23 +0530

    Create VARIABLES.c

---
+++ VARIABLES/VARIABLES.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    // character
+char a = 'y';
+    // float
+float b = 7.5;
+    // number
+int c = 5;
+
+/*
+Now printing character , float and int....
+\n --->> gives space
+*/
+
+
+printf("The Value Of character is %c \n", a);
+printf("The Value Of float is %f \n",b);
+printf("The Value Of integer is %d \n",c);
+    return 0;
+}
