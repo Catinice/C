bd02f2a83d7415313e82488d59ed691fcabbd6e0
Fastboot <premdevgan9@gmail.com>
2021-01-06 00:44:41 +0530

    adding daily stuff

---
+++ ADD_2_NUMBERS/add_2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+  // Assigning Variables
+    int num_1 = 69;
+    int num_2 = 9;
+    
+    
+  // Printing The Sum
+    printf("The sum of Two numbers is: %d",num_1 + num_2);
+    return 0;
