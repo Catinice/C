1f3e8dad0e2d4bfbeb22ef29010182bacf778e68
Fastboot <premdevgan9@gmail.com>
2021-01-06 01:16:23 +0530

    Take input from user and return sum...

---
+++ USER_INPUT/sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    
+  // Creating Variables 
+    int a , b;
+  // Taking Input From User ...
+    printf("Enter Number 1..... \n ");
+    printf("Enter Number 2..... \n ");
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+  // Printing The Sum
+    printf("The sum of Two numbers is: %d",a + b);
+    return 0;
+}
