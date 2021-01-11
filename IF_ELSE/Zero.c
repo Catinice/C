899775ea5ce009b9aebb7a904535b348a4e5f026
CatInIce69 <premdevgan9@gmail.com>
2021-01-11 13:35:58 +0530

    Fixing flaw

--- IF_ELSE/Zero.c
+++ IF_ELSE/Zero.c
@@ -7,13 +7,18 @@ int main()
     scanf("%d", &a);
     
     if(a==0){
-    printf("The number Is Zero!");
+    printf("The number Is Zero!\n");
  }
     if(a>0){
-    printf("The number is positive !");
+    printf("The number is positive !\n");
 }
-if(a<0){
-    printf("The number is Negative !");
+    if(a<0){
+    printf("The number is Negative !\n");
+}
+
+    else{
+    printf("The Entered String is not A number");
+    
 }
     return 0;
 }
