20617aeb686e405ec49a3435230051f6325601c4
CatInIce69 <premdevgan9@gmail.com>
2021-01-13 09:18:59 +0530

    Percentage.c : Adding Comments

--- USER_INPUT/percentage.c
+++ USER_INPUT/percentage.c
@@ -2,20 +2,35 @@
 
 int main()
 {
+
+/* 
+This program Will tell The percentage according to the marks obtained out of maximum marks.
+*/
+    // Taking variables 
     float hin, eng, math, sci, ss, total, totalm, percentage,c;
-    printf("Enter Max Marks Obtained In One Test:\n");
+    
+    // Calculating Max marks.
+    printf("Enter Max Marks Obtained In One Test:\n"); 
     scanf("%f",&total);
     totalm = 5*total;
+    
+    // Taking input of Marks Obtained.
     printf("Enter Marks In Subject 1 (Hindi for eg):\n");
     scanf("%f",&hin);
+    
     printf("Enter Marks In Subject 2 (English for eg):\n");
     scanf("%f",&eng);
+    
     printf("Enter Marks In Subject 3 (Maths for eg):\n");
     scanf("%f",&math);
+    
     printf("Enter Marks In Subject 4 (Science for eg):\n");
     scanf("%f",&sci);
+    
     printf("Enter Marks In Subject 5 (Social Science for eg): \n");
     scanf("%f",&ss);
+    
+    // Calculating Percentage.
     percentage = (hin + eng + math + sci + ss)*100 / totalm;
     printf("THE PERCENTAGE IS : %f", percentage);
 
