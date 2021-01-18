fdc5e513ec2d2977b2bcb36ecc87de58f19ab4ca
CatInIce69 <premdevgan9@gmail.com>
2021-01-18 16:11:42 +0530

    Create Do_while.c
    Changing Directory name and Creating do_while.c

---
+++ LOOPS/Do_while.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=0;
+    int n;
+    printf("Enter the value of n\n");
+    scanf("%d", n);
+    do{
+    printf("the value is %d \n",i); 
+    i++;
+    }while (i < n);
+
+    return 0;
+} 
