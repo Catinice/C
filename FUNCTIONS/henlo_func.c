1009965a35236890b28d3261cd37711af9dd64ab
CatInIce69 <premdevgan9@gmail.com>
2021-02-03 00:45:01 +0530

    henlo functions

---
+++ FUNCTIONS/henlo_func.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void gudm();
+void guda();
+void gudn();
+int main()
+{
+    printf("*******\n\n");
+    gudm();
+    guda();
+    gudn();
+    printf("\n\n*******\n");
+    return 0;
+}
+void gudm() {
+    printf("good morning :)\n");
+}
+void guda() {
+    printf("good afternoon :)\n");
+}
+void gudn() {
+    printf("good night :)\n");
+}
