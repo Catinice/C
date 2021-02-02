82f601c13405beb20176aeecf9c5bd7a8b079e2c
CatInIce69 <premdevgan9@gmail.com>
2021-02-03 00:43:03 +0530

    Add files via upload

---
+++ LOOPS/factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+   
+   int n , fac=1;
+   scanf("%d", &n);
+   for(int i =1; i<=n ; i++){
+   fac *= i;
+   
+}
+    
+    printf("%d\n", fac);
+    return 0;
+}
