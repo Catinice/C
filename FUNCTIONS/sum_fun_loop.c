1b77c4b5ac61d9ceb55878ef33a34ad7274eb444
catinice <premdevgan9@gmail.com>
2021-03-11 11:33:46 +0530

    IDK

--- FUNCTIONS/sum_fun_loop.c
+++ FUNCTIONS/sum_fun_loop.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
-int sum(int a , int b);
+int sum(a ,b);
 int main(){
     int num1;
     int num2;
     int result;
 
-    scanf("Enter Number 1: \n" , &num1);
-    scanf("Enter Number 2: \n" , &num2);
+    printf("Enter Number 1: \n");
+    printf("Enter Number 2: \n");
+    scanf("%d", &num1);
+    scanf("%d",&num2);
     
     result = sum( num1 , num2 );
     printf("The sum is %d \n",result);
-    return 0;
 }
-int sum(int a , int b){
+int sum(a , b){
     int result;
     result = (a + b); 
     return result;
