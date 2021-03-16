c2bf0cc6dd1d18eacf0c4fb2df2d45a6fa60f40c
CatInIce <premdevgan9@gmail.com>
2021-03-17 02:27:19 +0530

    Prime_func.c: Format

--- FUNCTIONS/prime_func.c
+++ FUNCTIONS/prime_func.c
@@ -3,26 +3,26 @@ int check(int);
 int main()
 {
     int n,prime;
-	printf("\n\n Function : check whether a number is prime number or not :\n");
-	printf("---------------------------------------------------------------\n");    	
+    printf("\n\n Function : check whether a number is prime number or not :\n");
+    printf("---------------------------------------------------------------\n");
     printf(" Input a positive number : ");
-   scanf("%d",&n);
+    scanf("%d",&n);
+    
     prime = check(n);
-   if(prime==1)
-   printf(" The number %d is a prime number.\n",n);
-   else
-   printf(" The number %d is not a prime number.\n",n);
-   return 0;
+    if(prime==1)
+        printf(" The number %d is a prime number.\n",n);
+    else
+        printf(" The number %d is not a prime number.\n",n);
+    return 0;
 }
 int check(int n)
 {
-   int a=2;
+    int a=2;
     while(a<n) {
-      if(n%a==0)
-     return 0;
+        if(n%a==0)
+            return 0;
         else
-   a++;
-       }
-    return 1;
+            a++;
     }
-        
+    return 1;
+}
